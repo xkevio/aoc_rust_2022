e4054029b76421a948d3a7081c6a27787a8222b0
xkevio <xkeviota@gmail.com>
2022-12-01 13:17:27 +0100

    Beautify itertools solution

--- src/days/day1_itertools.rs
+++ src/days/day1_itertools.rs
@@ -2,18 +2,13 @@ use itertools::Itertools;
 
 const INPUT: &str = include_str!("../../input/day1.txt");
 
-fn parse_input() -> Vec<usize> {
-    INPUT
+fn main() {
+    let input: Vec<usize> = INPUT
         .split("\n\n")
         .map(|cal| cal.lines().map(|l| l.parse::<usize>().unwrap()).sum())
         .sorted_by(|a, b| Ord::cmp(&b, &a))
-        .collect()
-}
+        .collect();
 
-pub fn part1() -> usize {
-    parse_input()[0]
-}
-
-pub fn part2() -> usize {
-    parse_input().iter().take(3).sum()
+    println!("Part 1: {}", input[0]);
+    println!("Part 2: {}", input[0] + input[1] + input[2]);
 }
