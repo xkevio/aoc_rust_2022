1274bd71174696c75dd9f42c83d1311fc50924d5
xkevio <xkeviota@gmail.com>
2022-12-03 23:51:07 +0100

    Simplify day 3

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,26 +1,25 @@
 use itertools::Itertools;
-use std::collections::HashSet;
 
 const INPUT: &str = include_str!("../../input/day3.txt");
+
+#[inline(always)]
+fn letter_to_prio(ch: char) -> usize {
+    if ch.is_ascii_uppercase() {
+        (27 + (ch as u8 - b'A')) as usize
+    } else {
+        (1 + (ch as u8 - b'a')) as usize
+    }
+}
 
 pub fn part1() -> usize {
     INPUT
         .lines()
         .map(|l| {
             let (a, b) = l.split_at(l.len() / 2);
-
-            let a_set: HashSet<char> = HashSet::from_iter(a.chars());
-            let b_set: HashSet<char> = HashSet::from_iter(b.chars());
-
-            a_set
-                .intersection(&b_set)
-                .map(|&a| {
-                    if a.is_ascii_uppercase() {
-                        (27 + (a as u8 - b'A')) as usize
-                    } else {
-                        (1 + (a as u8 - b'a')) as usize
-                    }
-                })
+            a.chars()
+                .filter(|ch| b.contains(*ch))
+                .dedup()
+                .map(letter_to_prio)
                 .sum::<usize>()
         })
         .sum()
@@ -31,22 +30,10 @@ pub fn part2() -> usize {
         .lines()
         .tuples()
         .map(|(a, b, c)| {
-            let a_set: HashSet<char> = HashSet::from_iter(a.chars());
-            let b_set: HashSet<char> = HashSet::from_iter(b.chars());
-            let c_set: HashSet<char> = HashSet::from_iter(c.chars());
-
-            a_set
-                .intersection(&b_set)
-                .copied()
-                .collect::<HashSet<char>>()
-                .intersection(&c_set)
-                .map(|&a| {
-                    if a.is_ascii_uppercase() {
-                        (27 + (a as u8 - b'A')) as usize
-                    } else {
-                        (1 + (a as u8 - b'a')) as usize
-                    }
-                })
+            a.chars()
+                .filter(|ch| b.contains(*ch) && c.contains(*ch))
+                .dedup()
+                .map(letter_to_prio)
                 .sum::<usize>()
         })
         .sum()
