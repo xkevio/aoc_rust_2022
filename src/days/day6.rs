12f798049d8a34a45f10ee8a44426a243677d381
xkevio <xkeviota@gmail.com>
2022-12-06 13:51:24 +0100

    Improve day 6 with `position()` iterator function

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -4,16 +4,11 @@ const INPUT: &str = include_str!("../../input/day6.txt");
 
 fn get_processed_amount(amount: usize) -> usize {
     INPUT
-        .char_indices()
+        .chars()
         .collect_vec()
         .windows(amount)
-        .find_map(|t| {
-            if t.iter().unique_by(|(_, c)| c).count() == amount {
-                Some(t.last().unwrap().0 + 1)
-            } else {
-                None
-            }
-        })
+        .position(|w| w.iter().all_unique())
+        .map(|i| i + amount)
         .unwrap()
 }
 
