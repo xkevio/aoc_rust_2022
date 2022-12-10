2f76ab9cb9a7db8283e53531c6d21eb0fada8f6e
xkevio <xkeviota@gmail.com>
2022-12-10 20:55:04 +0100

    Shorten day 10

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -2,14 +2,6 @@ const INPUT: &str = include_str!("../../input/day10.txt");
 
 type Screen = Vec<Vec<char>>;
 
-fn check_cycle(cy_count: i32, x: i32) -> i32 {
-    if (cy_count - 20) % 40 == 0 {
-        cy_count * x
-    } else {
-        0
-    }
-}
-
 fn draw_sprite_on_screen(current_image: &Screen, x: i32, cy_count: i32) -> Screen {
     let mut screen = current_image.clone();
 
@@ -17,26 +9,24 @@ fn draw_sprite_on_screen(current_image: &Screen, x: i32, cy_count: i32) -> Scree
     let col = (cy_count - 1) % 40;
 
     if x == col || x - 1 == col || x + 1 == col {
-        screen[row as usize][col as usize] = '#';
+        screen[row as usize][col as usize] = '█';
     }
 
     screen
 }
 
+#[rustfmt::skip]
 fn solve() -> (i32, Screen) {
-    let mut screen = vec![vec!['.'; 40]; 6];
+    let mut screen = vec![vec![' '; 40]; 6];
     let (_, signal, _) = INPUT.lines().fold((0, 0, 1), |mut acc, el| {
         let (ins, val) = el.split_at(4);
+        let iters = if ins == "noop" { 1 } else { 2 };
 
-        acc.0 += 1;
-        acc.1 += check_cycle(acc.0, acc.2);
-        screen = draw_sprite_on_screen(&screen, acc.2, acc.0);
-
-        if ins == "addx" {
+        for i in 0..iters {
             acc.0 += 1;
-            acc.1 += check_cycle(acc.0, acc.2);
+            acc.1 += if (acc.0 - 20) % 40 == 0 { acc.0 * acc.2 } else { 0 };
             screen = draw_sprite_on_screen(&screen, acc.2, acc.0);
-            acc.2 += val.trim().parse::<i32>().unwrap();
+            acc.2 += (i > 0).then(|| val.trim().parse::<i32>().unwrap()).unwrap_or(0);
         }
 
         acc
