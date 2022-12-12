f4123c17250bf102600e5874cb3033843e90af3f
xkevio <xkeviota@gmail.com>
2022-12-12 21:27:17 +0100

    Finish day 12

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,5 +1,5 @@
-use std::collections::{HashMap, BinaryHeap};
 use itertools::Itertools;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 const INPUT: &str = include_str!("../../input/day12.txt");
 
@@ -9,36 +9,32 @@ type AdjacencyList = HashMap<Element, Vec<Element>>;
 fn get_possible_neighbors(current_element: Element, grid: &[Vec<Element>]) -> Vec<Element> {
     let mut neighbors = Vec::<Element>::new();
     let (row, col) = current_element.0;
-    let value = current_element.1;
 
-    if let Some(up) = grid.get(row - 1) {
-        if let Some(upp) = up.get(col) {
-            if upp.1 as u8 == value as u8 + 1 {
-                neighbors.push(*upp);
+    let transform_value = |c: char| {
+        if c == 'S' {
+            'a'
+        } else if c == 'E' {
+            'z'
+        } else {
+            c
+        }
+    };
+
+    let value = transform_value(current_element.1);
+    for offset in [-1, 1] {
+        if let Some(up) = grid.get((row as i32 + offset) as usize) {
+            let v_upp = transform_value(up[col].1);
+            if v_upp as u8 <= value as u8 + 1 {
+                neighbors.push(up[col]);
             }
         }
     }
 
-    if let Some(up) = grid.get(row + 1) {
-        if let Some(upp) = up.get(col) {
-            if upp.1 as u8 == value as u8 + 1 {
-                neighbors.push(*upp);
-            }
-        }
-    }
-
-    if let Some(up) = grid.get(row) {
-        if let Some(upp) = up.get(col - 1) {
-            if upp.1 as u8 == value as u8 + 1 {
-                neighbors.push(*upp);
-            }
-        }
-    }
-
-    if let Some(up) = grid.get(row) {
-        if let Some(upp) = up.get(col + 1) {
-            if upp.1 as u8 == value as u8 + 1 {
-                neighbors.push(*upp);
+    for offset in [-1, 1] {
+        if let Some(up) = grid[row].get((col as i32 + offset) as usize) {
+            let v_upp = transform_value(up.1);
+            if v_upp as u8 <= value as u8 + 1 {
+                neighbors.push(*up);
             }
         }
     }
@@ -62,27 +58,61 @@ fn build_adjacency_list() -> AdjacencyList {
 
     for l in &grid {
         for el in l {
-            let neighbors = get_possible_neighbors(*el, &grid);
-            adjacency_list.insert(*el, neighbors);
+            adjacency_list.insert(*el, get_possible_neighbors(*el, &grid));
         }
     }
 
     adjacency_list
 }
 
-fn bfs(start_element: Element, goal: Element, adjacency_list: &AdjacencyList) -> Option<usize> {
-    let mut queue = BinaryHeap::<Element>::new();
-    let mut distance: Option<usize> = None;
+fn bfs(start_element: &Element, adjacency_list: &AdjacencyList) -> Option<usize> {
+    let mut queue = VecDeque::new();
+    let mut distances = HashMap::<Element, Element>::new();
+    let mut explored = HashSet::<Element>::new();
 
-    
+    explored.insert(*start_element);
+    queue.push_back(start_element);
 
-    distance
+    while !queue.is_empty() {
+        let v = queue.pop_front();
+
+        if let Some(vv) = v {
+            if vv.1 == 'E' {
+                let mut cur = vv;
+                let mut counter = 0;
+
+                while let Some(e) = distances.get(cur) {
+                    counter += 1;
+                    cur = e;
+                }
+
+                return Some(counter);
+            }
+
+            for n in adjacency_list.get(vv).unwrap() {
+                if !explored.contains(n) {
+                    explored.insert(*n);
+                    distances.insert(*n, *vv);
+                    queue.push_back(n);
+                }
+            }
+        }
+    }
+
+    None
 }
 
 pub fn part1() -> usize {
-    0
+    bfs(&((20, 0), 'S'), &build_adjacency_list()).unwrap_or_default()
 }
 
 pub fn part2() -> usize {
-    0
+    let adjacency_list = build_adjacency_list();
+
+    adjacency_list
+        .keys()
+        .filter(|k| k.1 == 'a')
+        .map(|start| bfs(start, &adjacency_list).unwrap_or(usize::MAX))
+        .min()
+        .unwrap()
 }
