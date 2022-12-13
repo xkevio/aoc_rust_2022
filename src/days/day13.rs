e38c0807399c8be1c101c4882b53b7dcb653c099
xkevio <xkeviota@gmail.com>
2022-12-13 20:00:42 +0100

    Finish day 13

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,95 +1,79 @@
-use colored::Colorize;
+use serde::Deserialize;
 
 const INPUT: &str = include_str!("../../input/day13.txt");
 
-fn parse_line(line: &str) -> Vec<Vec<u32>> {
-    let mut bracket_stack = Vec::<char>::new();
-    let mut num_stack = Vec::<u32>::new();
-
-    let mut list: Vec<Vec<u32>> = Vec::new();
-
-    line.chars().for_each(|c| match c {
-        '[' => bracket_stack.push(c),
-        ']' => {
-            bracket_stack.pop();
-            list.push(num_stack.clone());
-            num_stack.clear();
-        }
-        _ => {
-            if let Some(v) = c.to_digit(10) {
-                num_stack.push(v);
-            }
-        }
-    });
-
-    list
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+#[serde(untagged)]
+enum Packet {
+    Num(usize),
+    Nested(Vec<Packet>),
 }
 
-fn check_order() -> usize {
-    let mut count = 0;
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
-    for (index, pair) in INPUT.split("\n\n").enumerate() {
-        let (first, second) = pair.split_once("\n").unwrap();
-
-        let first_list = parse_line(first);
-        let second_list = parse_line(second);
-
-        println!("{}", "FIRST LIST START".red().bold());
-        println!("{:?}", &first_list);
-        println!("{}", "FIRST LIST END\n".red().bold());
-
-        println!("{}", "SECOND LIST START".green().bold());
-        println!("{:?}", &second_list);
-        println!("{}", "SECOND LIST END\n".green().bold());
-
-        if first_list.iter().all(|v| v.is_empty()) && second_list.iter().all(|v| v.is_empty()) {
-            println!("both lists only consist of empty lists!");
-            if first_list.len() < second_list.len() {
-                println!("-> correct order, left list (empty) ran out of elements first!");
-                count += index + 1;
-            } else {
-                println!("-> wrong order or continue");
-            }
-            continue;
-        } 
-
-        'outer: for (a, b) in first_list.iter().zip(second_list.iter()) {
-            for i in 0..(a.len().max(b.len())) {
-                let first_el = a.get(i);
-                let second_el = b.get(i);
-
-                if first_el.is_none() && second_el.is_some() {
-                    count += index + 1;
-                    println!("-> correct order!, left list ran out of elements first\n");
-                    break 'outer;
-                }
-
-                if first_el.is_some() && second_el.is_none() {
-                    println!("-> wrong order!, right list ran out of elements first\n");
-                    break 'outer;
-                }
-
-                if first_el.unwrap() < second_el.unwrap() {
-                    count += index + 1;
-                    println!("-> correct order!, left element smaller than right element\n");
-                    break 'outer;
-                }
-
-                if first_el.unwrap() > second_el.unwrap() {
-                    println!("-> wrong order!, left element bigger than right element\n");
-                    break 'outer;
-                }
-            }
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self {
+            Packet::Num(a) => match other {
+                Packet::Num(b) => a.cmp(b),
+                Packet::Nested(_) => other.cmp(self).reverse(),
+            },
+            Packet::Nested(v) => match other {
+                Packet::Num(_) => v.cmp(&vec![(*other).clone()]),
+                Packet::Nested(w) => v.cmp(w),
+            },
         }
     }
+}
 
-    count
+fn parse_input() -> Vec<Packet> {
+    INPUT
+        .lines()
+        .filter_map(|l| serde_json::from_str(l).ok())
+        .collect()
+}
+
+fn solve() -> (usize, usize) {
+    let packets = parse_input();
+
+    let part1 = check_order(&packets);
+    let part2 = find_decoder_key(&packets);
+
+    (part1, part2)
+}
+
+fn check_order(packets: &[Packet]) -> usize {
+    packets
+        .chunks(2)
+        .enumerate()
+        .filter(|(_, a)| a[0] < a[1])
+        .fold(0, |acc, (pos, _)| acc + pos + 1)
+}
+
+fn find_decoder_key(packets: &[Packet]) -> usize {
+    let mut extra_packets = packets.to_vec();
+
+    let packet_two = Packet::Nested(vec![Packet::Nested(vec![Packet::Num(2)])]);
+    let packet_six = Packet::Nested(vec![Packet::Nested(vec![Packet::Num(6)])]);
+
+    extra_packets.push(packet_two.clone());
+    extra_packets.push(packet_six.clone());
+    extra_packets.sort();
+
+    let key_one = extra_packets.iter().position(|v| *v == packet_two).unwrap() + 1;
+    let key_two = extra_packets.iter().position(|v| *v == packet_six).unwrap() + 1;
+
+    key_one * key_two
 }
 
 pub fn part1() -> usize {
-    check_order()
+    solve().0
 }
 
 pub fn part2() -> usize {
-    0
+    solve().1
 }
